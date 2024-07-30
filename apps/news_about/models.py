acef1d68ef9137001aeb3e0a417261142721d771
themuhammadyor <muhammadyor.2022@gmail.com>
2024-07-30 19:47:41 +0500

    feat: news_about app is done!

--- apps/news_about/models.py
+++ apps/news_about/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from apps.shared.models import AbstractBaseModel
+from phone_field import PhoneField
+
+
+class News(AbstractBaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='news/')
+    phone = PhoneField(blank=True, help_text='Contact phone number')
+
+    def __str__(self):
+        return self.title
+
+
+class About(AbstractBaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='about/')
+
+    def __str__(self):
+        return self.title
